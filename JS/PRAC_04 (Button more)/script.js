936452cff7cc085e6047173d24b12c12fd9ceef7
3CSTVZZ <corp.of.alexeev@gmail.com>
2023-05-04 21:33:01 +0300

    PRAC_04 (Button more)

---
+++ JS/PRAC_04 (Button more)/script.js
@@ -0,0 +1,46 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".header__nav");
+let menuLinks = menu.querySelectorAll(".nav__link");
+
+burger.addEventListener(
+  "click",
+
+  function () {
+    burger.classList.toggle("burger--active");
+
+    menu.classList.toggle("header__nav--active");
+
+    document.body.classList.toggle("stop-scroll");
+  }
+);
+
+menuLinks.forEach(function (el) {
+  el.addEventListener(
+    "click",
+
+    function () {
+      burger.classList.remove("burger--active");
+
+      menu.classList.remove("header__nav--active");
+
+      document.body.classList.remove("stop-scroll");
+    }
+  );
+});
+
+const btnMore = document.querySelector(".articles__more");
+const articlesItems = document.querySelectorAll(".articles__item");
+
+btnMore = addEventListener(
+  "click",
+
+  () => {
+    articlesItems.forEach((el) => {
+      el.classList.add("articles__item_visible");
+    });
+
+    btnMore
+      .closest(".articles__center")
+      .classList.add("articles__center_hidden");
+  }
+);
