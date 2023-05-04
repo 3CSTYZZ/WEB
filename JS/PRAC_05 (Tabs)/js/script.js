2dcf9573f6b93b71a2f55dded357bbabe62bb1fe
3CSTVZZ <corp.of.alexeev@gmail.com>
2023-05-04 21:51:27 +0300

    PRAC_05 (Tabs)

---
+++ JS/PRAC_05 (Tabs)/js/script.js
@@ -0,0 +1,14 @@
+let tabsBtn = document.querySelectorAll('.tabs-nav__btn');
+let tabsItem = document.querySelectorAll('.tabs-item');
+
+tabsBtn.forEach(function(element){
+  element.addEventListener('click', function(e){
+    const path = e.currentTarget.dataset.path;
+ 
+    tabsBtn.forEach(function(btn){ btn.classList.remove('tabs-nav__btn--active')});
+    e.currentTarget.classList.add('tabs-nav__btn--active');
+ 
+    tabsItem.forEach(function(element){ element.classList.remove('tabs-item--active')});
+    document.querySelector(`[data-target="${path}"]`).classList.add('tabs-item--active');
+  });
+});
