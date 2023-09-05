10b6372f685cf4fbbc2837867e40e3d380832634
3CSTVZZ <corp.of.alexeev@gmail.com>
2023-09-05 20:40:39 +0300

    PRAC_09 (Digits)

---
+++ JS/PRAC_09 (Digits)/index.js
@@ -0,0 +1,18 @@
+let digit = 66;
+let fract = -3.05;
+let exp = 2e10;
+let expSmall = 2e-3;
+let expFract = 1.5e3;
+let expFractSmall = -1.5e-3;
+let fract1 = 25.;
+let fract2 = .25;
+
+// двоичная система
+let dec = 9;
+let bin = 0b1001;
+
+// 8
+let oct = 0o10;
+
+// 16
+let x = 0x1b;
