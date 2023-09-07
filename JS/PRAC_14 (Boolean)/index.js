e89e920d6cfc6485f19ef8b3297ed14cf5f18750
3CSTVZZ <corp.of.alexeev@gmail.com>
2023-09-07 18:51:07 +0300

    PRAC_14 (Boolean)

---
+++ JS/PRAC_14 (Boolean)/index.js
@@ -0,0 +1,19 @@
+let yey = true;
+let noo = false;
+
+let yes = 5 > 3;
+let no = 5 < 3;
+
+// инвентирование
+let alsoNo = !yes;
+
+let x = 10;
+let y = 20;
+
+if (x > y) {
+  console.log("x");
+}
+
+if (x < y) {
+  console.log("y");
+}
