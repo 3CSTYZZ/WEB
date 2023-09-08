148d3c0f5077ef4179e7e8cab2f8574a8d7d365c
3CSTVZZ <corp.of.alexeev@gmail.com>
2023-09-08 19:14:41 +0300

    PRAC_17 (Array)

---
+++ JS/PRAC_17 (Array)/index.js
@@ -0,0 +1,16 @@
+let fruits = ["Apple", "Strawberry", "Orange"];
+
+let alphabet = [];
+
+// добваить в конце массива
+alphabet.push("Г");
+alphabet.push("Д");
+alphabet.push("Е", "Ж", "З");
+
+// добавить в начале массива
+alphabet.unshift("В");
+alphabet.unshift("А", "Б");
+
+console.log(alphabet);
+
+alphabet.length; // 8
